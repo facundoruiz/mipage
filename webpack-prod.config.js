bacfdf2e5c3709d6fb286311185d52c521e36158
Facundo Ruiz <1211581+facundoruiz@users.noreply.github.com>
2023-06-05 22:47:19 -0300

    nueva config

--- webpack-prod.config.js
+++ webpack-prod.config.js
@@ -1,6 +1,16 @@
 const path = require('path');
+const autoprefixer = require('autoprefixer')
 const HtmlWebpackPlugin = require("html-webpack-plugin"); 
 const MiniCssExtractPlugin = require('mini-css-extract-plugin'); //extrae css
+
+const webpack = require('webpack')
+const dotenv = require('dotenv')
+
+const env = dotenv.config().parsed
+const envKeys = Object.keys(env).reduce((prev, next) => {
+  prev[`process.env.${next}`] = JSON.stringify(env[next])
+  return prev
+}, {})
 
 module.exports = {
     mode: "production",
@@ -14,23 +24,60 @@ module.exports = {
     },
     module: {
         rules: [
-
             {
                 test: /\.css$/i,
                 use: [MiniCssExtractPlugin.loader, 'css-loader'],
             },
+            {
+                test: /\.(scss)$/,
+                use: [
+                  {
+                    // Adds CSS to the DOM by injecting a `<style>` tag
+                    loader: 'style-loader'
+                  },
+                  {
+                    // Interprets `@import` and `url()` like `import/require()` and will resolve them
+                    loader: 'css-loader'
+                  },
+                  {
+                    // Loader for webpack to process CSS with PostCSS
+                    loader: 'postcss-loader',
+                    options: {
+                      postcssOptions: {
+                        plugins: () => [
+                          autoprefixer
+                        ]
+                      }
+                    }
+                  },
+                  {
+                    // Loads a SASS/SCSS file and compiles it to CSS
+                    loader: 'sass-loader'
+                  }
+                ]
+              }
         ],
     },
     plugins: [
+        new webpack.DefinePlugin(envKeys),
+
         new MiniCssExtractPlugin({
             filename: 'css/[name].min.css'
         })
          ,
          new HtmlWebpackPlugin({
-         	title: 'INICIO',
+         	title: 'PRUEBA',
+             hash: true,
          	filename: '../index.html',
-         	template: 'src/template.html'
+          template: 'src/index.html'
+       //  	template: 'src/template.html'
          }),
+     
+        new HtmlWebpackPlugin({
+            title: 'Registrar',
+            filename: '../registrar.html',
+            template: 'src/registro.html'
+        }),
          new HtmlWebpackPlugin({
             title: 'Fuera de Servicio',
             filename: '../offline.html',
